867b26323ad4990710604a01caa1d27fd95fdd67
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-08-04 16:59:43 -0300

    feat: Implementa funcionalidade de exibir/ocultar senha
    - Adiciona botão de alternância dentro do campo de senha
    - Implementa lógica para alternar visibilidade da senha
    - Atualiza estilo do input para acomodar o botão internamente
    - Melhora a experiência do usuário ao interagir com o campo de senha

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,8 +1,30 @@
-import { InputHTMLAttributes } from "react"
+import { InputHTMLAttributes, useState } from "react"
+import { FaEyeSlash } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
 
 export const Input = (props: InputProps) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    if (props.type === "password") {
+        return (
+            <div className="relative">
+                <input
+                    className="border-0 h-9 rounded-md outline-none px-2 mb-3 w-full"
+                    {...props}
+                    type={showPassword ? "text" : "password"}
+                />
+                <button
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-0 p-2 h-9 text-black bg-transparent text-white rounded-r outline-none"
+                >
+                    {showPassword ? <IoEyeSharp /> : <FaEyeSlash />}
+                </button>
+            </div>
+        )
+    }
+
     return (
         <input
             className="border-0 h-9 rounded-md outline-none px-2 mb-3 w-full"

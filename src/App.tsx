6a9bf99a429c4eabded459fddbe82d4b8fd3114e
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-08-07 18:43:29 -0300

    refactor: remover dependência do react-toastify
    - Remove a biblioteca react-toastify do projeto
    - Atualiza sistema de notificações para uma solução interna
    - Ajusta componentes afetados pela remoção
    - Atualiza documentação relacionada

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes/router"
-import { ToastContainer } from "react-toastify"
 
 const App = () => {
   return (
     <>
-      <ToastContainer autoClose={3000} limit={1} />
       <RouterProvider router={router} />
     </>
   )

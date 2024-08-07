b95137890e78bef4fcdfaf4dc87285560881afb1
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-08-07 20:47:15 -0300

    "fix: Corrige comportamento do botão 'exibir senha' Altera o tipo do botão de 'submit' para 'button' para evitar o envio acidental do formulário ao pressionar Enter. Agora, o botão apenas alterna a visibilidade da senha conforme esperado."

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -9,19 +9,20 @@ export const Input = (props: InputProps) => {
 
     if (props.type === "password") {
         return (
-            <div className="relative">
+            <div className="relative" onSubmit={() => setShowPassword(false)}>
                 <input
                     className="border-0 h-9 rounded-md outline-none px-2 mb-3 w-full"
                     {...props}
                     type={showPassword ? "text" : "password"}
                 />
                 <button
+                    type="button"
                     onClick={() => setShowPassword(!showPassword)}
                     className="absolute right-0 p-2 h-9 text-black bg-transparent rounded-r outline-none"
                 >
                     {showPassword ? <IoEyeSharp /> : <FaEyeSlash />}
                 </button>
-            </div>
+            </div >
         )
     }
 
